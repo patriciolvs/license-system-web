50da742e8098b9ddb0c624e8be6be748a950287f
patriciolvs <lopat.jatai@gmail.com>
2025-04-21 23:58:38 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 // Substitua pelo URL do seu Web App
-const API_URL = "https://script.google.com/macros/s/AKfycby9wmolJyXwnQNJqLMZPnAgAbMnk3Og2XEAjin3yRhLN1GDtrvmLV3ahm5mWCSf66dcPw/exec"; // Ex.: "https://script.google.com/macros/s/[SEU_ID]/exec"
+const API_URL = "https://script.google.com/macros/s/1QlhykLWCENj5XvtNGg0_Kezeuarjb-flSHXOxPJhNWn4Nw8mr1Zs8Teh/exec"; // Ex.: "https://script.google.com/macros/s/[SEU_ID]/exec"
 
 // Lista predefinida de e-mails
 const emailList = [
